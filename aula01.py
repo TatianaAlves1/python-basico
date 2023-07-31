e1dd1a5f69db9ec96ae6fd54146815e507ff1e9b
TatianaAlves1 <tatipublisher@gmail.com>
2023-07-31 11:12:28 -0400

    formatação de percetual

--- aula01.py
+++ aula01.py
@@ -25,11 +25,17 @@ print("Multiplicação: ",valor1*valor2,"\n")
 print("Divisão: ",valor1/valor2,"\n")
 print("Divisão sem decimais ",valor1//valor2,"\n")
 
+#alinhamentos
+print(f"{'Seja bem-vindo':^50}")
+
 #f strings do python
 print(f"O 1º valor é {valor1:.2f} e o segundo valor é {math.trunc(valor2)}")
 print(f"O 1º valor é {valor1:.2f} e o segundo valor é {valor2:.2f} e soma é {valor1+valor2:.2f}")
 #usando marcadores
 print("O valor 1 é %s e o valor2 é %s"%(valor1,valor2))
 #Usando format
-print("O 1º valor é {} e o segundo valor é {}".format(valor1,valor2))
+print("O 1º valor é {:d} e o segundo valor é {:.2f}".format(valor1,valor2),end="\n**********\n")
+print(f"{'Todas as variáveis':^50}")
+print(valor1,valor2,sep=" - ")
 print(datetime.today())
+print(math.sqrt(valor1))
